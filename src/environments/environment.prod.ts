9d743911265ff98786a97b842114e7d7ac9be441
Swagatika-Panda <hhgp2@handholdersglobal.com>
2022-03-22 10:13:32 +0530

    project set up and theme integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDbOqD9lPbsWv8uRRyxS3hIZcHolqKbwgI',
+    authDomain: 'angular-shop101.firebaseapp.com',
+    projectId: 'angular-shop101',
+    storageBucket: 'angular-shop101.appspot.com',
+    messagingSenderId: '906271240938',
+    appId: '1:906271240938:web:c8ac75e0634b590e47dd93',
+    measurementId: 'G-1GD8YBXWVB',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDbOqD9lPbsWv8uRRyxS3hIZcHolqKbwgI',
+    authDomain: 'angular-shop101.firebaseapp.com',
+    projectId: 'angular-shop101',
+    storageBucket: 'angular-shop101.appspot.com',
+    messagingSenderId: '906271240938',
+    appId: '1:906271240938:web:c8ac75e0634b590e47dd93',
+    measurementId: 'G-1GD8YBXWVB',
+  },
 };
 
 /*
